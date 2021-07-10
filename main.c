cf95da4d7675b31ba115230b358487e54b9a459d
Usurupati Likhith <likhithstark369@gmail.com>
2021-07-10 16:14:51 +0530

    first comiit

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
+#include<stdlib.h>
 int main()
 {
-    
+    FILE *f;
+    char str[100000];
+    printf("enter:\n");
+    f= open("code.txt","w");
+    fscanf(f,"%[^\0]%s",str);
+    fclose(f); 
 }
