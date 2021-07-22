91ccaaf8b14c1e65d30c795ee388a633efb85356
Usurupati Likhith <likhithstark369@gmail.com>
2021-07-22 19:34:39 +0530

    Fourth commit

--- main.c
+++ main.c
@@ -2,11 +2,49 @@
 #include<string.h>
 #include<conio.h>
 #include<stdlib.h>
+FILE *f;
+void NLOC();
+void write();
 int main()
 {
-    FILE *f;
-    char str[100000];int i=0;
-    printf("Enter Your Code(type and enter _ to end input):\n");
+    char ch,c;
+    do
+    {
+        printf("\t\t\t\tWelcome\n");
+        printf("\t\t\tLEXICAL ANALYSIS\n");
+        printf("1.Modify Code\n2.NLOC\n");
+        printf("3.Token Count\n4.Complexity Analysis\n");
+        printf("Enter your choice: ");
+        c= getch();
+        switch (c)
+        {
+        case '1':
+            printf("\nunder development!!");
+            write();
+            break;
+        case '2':
+            NLOC();
+            break;
+        case '3':
+            printf("under development!!");
+            break;
+        case '4':
+            printf("under development!!");
+            break;
+        default:
+            printf("\nEnter Proper choice!!");
+            break;
+        }
+        printf("\nWant to continue?[y:n]");
+        ch= getch();
+    } while (ch!='n');
+}
+
+void write()
+{
+    char str[100000]; 
+    int i=0;
+    printf("\nEnter Your Code(type and enter _ to end input):\n");
     f= fopen("code.txt","w");
     if(f == NULL)
     printf("buffer!!");
@@ -21,8 +59,14 @@ int main()
     }    
     str[i]=feof(f);
     fprintf(f,"%s",str);
+    if(f==NULL)
+    printf("Code saved to code.txt in disk!!");
     fclose(f); 
-    int count = 0;i=0;
+}
+
+void NLOC()
+{
+    int count = 0;
     f=fopen("code.txt","r");
     char chr;
     chr= getc(f);
